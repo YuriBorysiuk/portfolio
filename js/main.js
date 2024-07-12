f8cd6807b15a97a22d527fe67fcf40721bee401c
YuriBorysiuk <65165493+YuriBorysiuk@users.noreply.github.com>
2024-07-13 02:00:54 +0300

    add save dark mode in localStorage

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 const btnDarkMode = document.querySelector('.dark-mode-btn');
 
+// Проверка темной темы в LocalStorage
+if (localStorage.getItem('darkMode') === 'dark') {
+	btnDarkMode.classList.add('dark-mode-btn--active');
+	document.body.classList.add('dark');
+}
+
+//Включение ночного режима по кнопке
 btnDarkMode.onclick = function () {
 
 	btnDarkMode.classList.toggle('dark-mode-btn--active');
-	document.body.classList.toggle('dark')
+	const isDark = document.body.classList.toggle('dark');
+
+	if (isDark) {
+		localStorage.setItem('darkMode', 'dark')
+	} else {
+		localStorage.setItem('darkMode', 'light');
+	}
 }
